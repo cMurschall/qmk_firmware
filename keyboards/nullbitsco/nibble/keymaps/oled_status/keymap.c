668dc84f448f1144a51c6773e3ee14b113f6917b
Christian Murschall <christian@murschall.net>
2023-05-29 15:10:18 +0200

    get rid of via which caused havoc for left gr key added snake layer

--- keyboards/nullbitsco/nibble/keymaps/oled_status/keymap.c
+++ keyboards/nullbitsco/nibble/keymaps/oled_status/keymap.c
@@ -20,14 +20,15 @@
 #endif
 
 
-bool has_layer_changed = false;
-static uint8_t current_layer;
+// bool has_layer_changed = false;
+// static uint8_t current_layer;
 
-enum layer_names { _MA, _FN };
+enum layer_names { _MA, _FN, _SNAKE };
 
 enum custom_keycodes {
     KC_CUST = SAFE_RANGE,
 };
+
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -36,14 +37,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_MUTE, KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
         KC_F10, KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGDN,
          KC_F9, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_END,
-         KC_F8, KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                    MO(_FN), KC_RALT, KC_RCTL, KC_LEFT,          KC_DOWN, KC_RGHT
+         KC_F8, KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                    KC_RALT, MO(_FN), KC_RCTL, KC_LEFT,          KC_DOWN, KC_RGHT
     ),
     [_FN] = LAYOUT_ansi(
-                 QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  KC_END,
-        RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        RGB_MOD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+                 QK_BOOT, KC_F1,   KC_F2,     KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  KC_END,
+        RGB_TOG, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        RGB_MOD, _______, _______,TO(_SNAKE), _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+        _______, _______, _______, _______,   _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
+        _______, _______, _______, _______,                     _______,                   _______, _______, _______, _______,          _______, _______
+    ),
+    [_SNAKE] = LAYOUT_ansi(
+                 TO(_MA), _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______,
-        _______, _______, _______, _______,                   _______,                   _______, _______, _______, _______,          _______, _______
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_UP  , _______,
+        _______, _______, _______, _______,                   _______,                   _______, _______, _______, KC_LEFT,          KC_DOWN, KC_RGHT
     ),
 };
 // clang-format on
@@ -81,6 +89,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }
             break;
     }
+
+#ifdef CONSOLE_ENABLE
+    uprintf("KL: kc: 0x%04X, col: %2u, row: %2u, pressed: %u, time: %5u, int: %u, count: %u\n", keycode, record->event.key.col, record->event.key.row, record->event.pressed, record->event.time, record->tap.interrupted, record->tap.count);
+#endif 
     return true;
 }
 
@@ -109,11 +121,11 @@ void matrix_scan_user(void) {
     matrix_scan_remote_kb();
 
 
-    // look for layer change
-    if (current_layer == layer) {
-        has_layer_changed = false;
-    } else {
-        has_layer_changed = true;
-        current_layer = layer;
-    }
+    // // look for layer change
+    // if (current_layer == layer) {
+    //     has_layer_changed = false;
+    // } else {
+    //     has_layer_changed = true;
+    //     current_layer = layer;
+    // }
 }
